7c19fa674d7b6a42df1bd977beafd66353480f2a
ahmedshamkh9999 <145130054+ahmedshamkh9999@users.noreply.github.com>
2026-09-23 17:13:38 +0300

    Delete serve.py

--- serve.py
+++
@@ -1,6 +0,0 @@
-from waitress import serve
-from app import app  # يستدعي التطبيق من ملف app.py
-
-if __name__ == '__main__':
-    print("POS Server running on http://0.0.0.0:5000")
-    serve(app, host='0.0.0.0', port=5000)
